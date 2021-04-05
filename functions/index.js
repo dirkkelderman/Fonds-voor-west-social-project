a70922b6e6e136b2956990240741f055d11cbc2b
Dirk Kelderman <dirkkelderman@gmail.com>
2021-04-05 16:05:01 +0200

    user authorization completed

--- functions/index.js
+++ functions/index.js
@@ -40,17 +40,47 @@ app.get('/screams', (req, res) => {
     .catch( err =>  console.error(err))
 })
 
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return res.status(403).json({ error: 'Unauthorized' });
+    }
 
-app.post('/scream', (req, res) => {
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log(decodedToken)
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get()
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next()
+        })
+        .catch(err => {
+            console.error('Error while verifying token', err);
+            return res.status(403).json(err);
+        })
+}
+
+// Post one scream
+app.post('/scream', FBAuth, (req, res) => {
+    if (req.body.body.trim() === '') {
+        return res.status(400).json({body: 'Body must not be empty'})
+    }
 
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
-    db
-        .collection('screams')
+    db.collection('screams')
         .add(newScream)
         .then(doc => {
             res.json({message: `document ${doc.id} created successfully`});
