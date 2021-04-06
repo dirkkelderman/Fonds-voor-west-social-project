de4899916db15c2fcf0ea13cc40299bd5e071f49
Dirk Kelderman <dirkkelderman@gmail.com>
2021-04-06 15:14:08 +0200

    refactoring code, excluded the API key in config.js

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    authDomain: "fonds-voor-west.firebaseapp.com",
+    projectId: "fonds-voor-west",
+    storageBucket: "fonds-voor-west.appspot.com",
+    messagingSenderId: "789083881359",
+    appId: "1:789083881359:web:9f1a8514ba64aa79fb15fa",
+    measurementId: "G-P63LYED9H3"
+  };
