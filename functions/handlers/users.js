e2739441a341f033b7b67c1b3e27cd61e92172f7
Dirk Kelderman <dirkkelderman@gmail.com>
2021-04-09 15:26:55 +0200

    Adding and getting user profile details completed

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -5,8 +5,9 @@ const config = require ('../util/config')
 const firebase = require('firebase')
 firebase.initializeApp(config)
 
-const { validateSignupData, validateLoginData } = require('../util/validators')
+const { validateSignupData, validateLoginData, reduceUserDetails } = require('../util/validators')
 
+// Signup user
 exports.signup = (req, res) => {
     const newUser = {
         email: req.body.email,
@@ -62,6 +63,7 @@ exports.signup = (req, res) => {
 
 }
 
+// Login user
 exports.login = (req, res) => {
     const user = {
         email: req.body.email,
@@ -90,6 +92,44 @@ exports.login = (req, res) => {
         }) 
 }
 
+//Add user details
+exports.addUserDetails = (req, res) => {
+    let userDetails = reduceUserDetails(req.body);
+
+    db.doc(`/users/${req.user.handle}`).update(userDetails)
+        .then(() => {
+            return res.json({message: 'Details added successfully'})
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({error: err.code})
+        })
+}
+
+//Get own user details
+exports.getAuthenticatedUser = (req, res) => {
+    let userData = {};
+    db.doc(`/users/${req.user.handle}`).get()
+    .then( doc => {
+        if(doc.exists){
+            userData.credentials = doc.data();
+            return db.collection('likes').where('userHandle', '==', req.user.handle).get();
+        }
+    })
+    .then(data => {
+        userData.likes = []
+        data.forEach((doc) => {
+            userData.likes.push(doc.data())
+        });
+        return res.json(userData)
+    })
+    .catch(err => {
+        console.error(err);
+        return res.status(500).json({error: err.code});
+    })
+}
+
+// Add profile picture for user
 exports.uploadImage = (req, res) => {
     const BusBoy = require('busboy');
     const path = require('path');
